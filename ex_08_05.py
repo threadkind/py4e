9dc92376e85c5c53ec391dc545868fcfa0158a98
threadkind <threadkind@gmail.com>
2020-08-17 01:17:20 -0500

    Add Ex 7.2

--- ex_08_05.py
+++ ex_08_05.py
@@ -11,4 +11,13 @@ if len(fname) < 1 : fname = "mbox-short.txt"
 fh = open(fname)
 count = 0
 
+for line in fh :
+    if not line.startswith("From") : continue
+    l = line.split(' ')
+
+    if len(l) > 2 :
+        email = line.split(' ')[1]
+        print(email)
+        count = count + 1
+
 print("There were", count, "lines in the file with From as the first word")
